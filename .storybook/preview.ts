d2e787dcc13259ab444eee2e99169b82556bfafc
Andrew Dyer <andrewdyer@outlook.com>
2024-01-10 22:49:12 +0000

    added preview dependencies

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,16 @@
-import { Preview } from "@storybook/react";
+import '@typedbootstrap/admin-styles/dist/css/styles.min.css';
+import { Preview } from '@storybook/react';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i
+            }
+        }
+    }
 };
 
 export default preview;
